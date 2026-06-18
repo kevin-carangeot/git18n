91aadd4032a81baba0ecf4dcdf357ffeba904993
Carangeot <kevin.carangeot@prestashop.com>
2026-06-18 18:11:09 +0200

    feat: whole language switch toggles locale on click

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -3,6 +3,12 @@ const { locale, setLocale, locales, t } = useI18n()
 
 const repoUrl = 'https://github.com/kevin-carangeot/git18n'
 const repoDisplay = repoUrl.replace('https://github.com/', '')
+
+const toggleLocale = () => {
+	const codes = locales.value.map((l) => l.code)
+	const next = codes[(codes.indexOf(locale.value) + 1) % codes.length]
+	if (next) setLocale(next)
+}
 </script>
 
 <template>
@@ -38,27 +44,25 @@ const repoDisplay = repoUrl.replace('https://github.com/', '')
 						<UIcon name="i-simple-icons-github" class="size-3.5" />
 						{{ repoDisplay }}
 					</a>
-					<div
-						role="group"
+					<button
+						type="button"
 						:aria-label="t('common.language')"
-						class="flex items-center gap-0.5 rounded-[9px] border border-slate-200 bg-white p-0.5 dark:border-white/10 dark:bg-white/5"
+						class="flex cursor-pointer items-center gap-0.5 rounded-[9px] border border-slate-200 bg-white p-0.5 transition hover:border-slate-300 dark:border-white/10 dark:bg-white/5 dark:hover:border-white/20"
+						@click="toggleLocale"
 					>
-						<button
+						<span
 							v-for="loc in locales"
 							:key="loc.code"
-							type="button"
-							:aria-pressed="locale === loc.code"
-							class="cursor-pointer rounded-[7px] px-2 py-1 text-xs font-semibold uppercase tracking-wide transition"
+							class="rounded-[7px] px-2 py-1 text-xs font-semibold uppercase tracking-wide transition"
 							:class="
 								locale === loc.code
 									? 'bg-emerald-600 text-white shadow-sm'
-									: 'text-slate-400 hover:text-slate-700 dark:hover:text-slate-200'
+									: 'text-slate-400'
 							"
-							@click="setLocale(loc.code)"
 						>
 							{{ loc.code }}
-						</button>
-					</div>
+						</span>
+					</button>
 					<UButton
 						to="/settings"
 						icon="i-heroicons-cog-6-tooth"
